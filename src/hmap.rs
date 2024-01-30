8a465b317bc142f529db925fdd9d6cf17bf46f86
Your Name <you@example.com>
2024-01-30 20:31:41 +0100

    Tests and optimization

--- src/hmap.rs
+++ src/hmap.rs
@@ -1,3 +1,4 @@
+// Yo this hashmap is lowkey usable
 use std::fmt::Display;
 use std::marker::PhantomData;
 use std::{ptr::null_mut, usize};
@@ -6,7 +7,7 @@ use std::hash::{Hash, Hasher};
 
 #[derive(Debug)]
 pub struct Map<K: Hash, T: Sized + Copy> {
-    pub bucket: Vec<*mut T>,
+    pub bucket: Box<Vec<*mut T>>,
     size: usize,
     a: f64,
     key_type: PhantomData<K>
@@ -14,16 +15,15 @@ pub struct Map<K: Hash, T: Sized + Copy> {
 
 impl<K: Hash, T: Copy + Sized + Display> Map<K, T> {
     pub fn new(size: usize) -> Self {
-        let mut v = Vec::with_capacity(size);
-        v.resize(size, null_mut());
-        Map {bucket: v, size, a: 0.351876846351345687, key_type: PhantomData}
+        let b = vec![null_mut(); size];
+        Map {bucket: b.into(), size, a: 0.351876846351345687, key_type: PhantomData}
     } 
-    pub fn calculate_hash(&mut self, t: &K) -> u64 {
+    fn calculate_hash(&mut self, t: &K) -> u64 {
         let mut s = DefaultHasher::new();
         t.hash(&mut s);
         s.finish()
     }
-    pub fn hash(&mut self, key: K) -> usize {
+    fn hash(&mut self, key: K) -> usize {
         let mut u = self.calculate_hash(&key);
         u = u / 1000000;
         let mut out = u as f64 * self.a;
@@ -44,5 +44,30 @@ impl<K: Hash, T: Copy + Sized + Display> Map<K, T> {
     }
     pub fn print(&mut self, key: K) {
         println!("{}", self.get(key));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Map;
+
+    #[test]
+    fn working_map() {
+        let mut m: Map<&str, i32> = Map::new(1000); // The number sets the size of the map, the maximum is a billion
+        m.set("foo", 5);
+        m.set("oofooo", 9);
+        m.set("bar", 4);
+        m.print("foo");
+        assert_eq!(m.get("foo"), 5);
+        assert_eq!(m.get("bar"), 4);
+    }
+
+    #[test]
+    fn working_generics() {
+        let mut m2: Map<i32, i32> = Map::new(1000);
+        m2.set(5, 8);
+        m2.set(4, 7);
+        assert_eq!(m2.get(5), 8);
+        assert_eq!(m2.get(4), 7)
     }
 }
